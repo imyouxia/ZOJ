86055c98641f51c0e37a2d431001e16390087ff3
armsword <root@linuxer.me>
2014-05-21 22:42:23 +0800

    完美立方-剪枝

---
+++ 1331.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int a,b,c,d;
+	for(a = 6; a <= 200; a++)
+	{
+		for(b = 2; b < a; b++)
+		{
+			for(c = b; c < a; c++)
+			{
+				for(d = c; d < a; d++)
+				{
+					if(a*a*a == (b*b*b + c*c*c + d*d*d))
+					{
+						printf("Cube = %d, Triple = (%d,%d,%d)\n",a,b,c,d);
+					}
+				}
+
+			}
+
+		}
+	}
+	return 0;
+}
+
